c43c4e4f518432498a41463c308c90b2376bf11e
Nathan Palatin <nath.palatin@gmail.com>
2023-05-28 14:40:25 -0300

    add user contexts and login data with routes

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { AppError } from '"@utils/AppError';
 
 const api = axios.create({
-  baseURL: "http://192.168.0.170:3333",
+  baseURL: "http://0.0.0.0:3333",
 });
 
 api.interceptors.response.use(
